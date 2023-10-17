f843d6a95d8d31290b77b3816e3375116954f12d
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-10-17 23:43:35 +0200

    Commit Final

--- app/src/main/java/com/juanlugg8/sendmessageviewbinding/AboutActivity.java
+++ app/src/main/java/com/juanlugg8/sendmessageviewbinding/AboutActivity.java
@@ -4,15 +4,41 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
+import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
+import com.danielstone.materialaboutlibrary.model.*;
 
-class AboutActivity extends com.danielstone.materialaboutlibrary.MaterialAboutActivity {
+class AboutActivity extends MaterialAboutActivity {
     @Override
     @NonNull
     protected MaterialAboutList getMaterialAboutList(@NonNull Context context) {
-        return new MaterialAboutList.Builder()
-                .build(); // This creates an empty screen, add cards with .addCard()
+        MaterialAboutCard.Builder cardBuilder = new MaterialAboutCard.Builder();
 
+        cardBuilder.addItem(new MaterialAboutTitleItem.Builder()
+                .text("Personal information")
+                .build());
+
+        cardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Autor")
+                .subText("Juan Luis Guerra Gennich")
+                //.icon(R.drawable.ic_about_info)
+                .build());
+
+        cardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Fork on GitHUB")
+                .subText("1.0.0")
+                //.icon(R.drawable.ic_about_info)
+                .build());
+
+        cardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Version")
+                .subText("1.0.0")
+                //.icon(R.drawable.ic_about_info)
+                .build());
+
+        return new MaterialAboutList.Builder()
+                .addCard(cardBuilder.build()).build();
     }
 
     @Override
